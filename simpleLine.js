85c8d4ff986e72f95f1d2113d344f87ba4f049d7
Sif <sifddin@hotmail.com>
2020-09-25 15:44:56 +0200

    push the first code

---
+++ simpleLine.js
@@ -0,0 +1,11 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        create: create,
+        update: update
+    }
+}
