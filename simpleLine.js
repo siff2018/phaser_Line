482da5db2bda1b89842aae435c34efffaf06dd4c
Sif <sifddin@hotmail.com>
2020-09-25 16:22:21 +0200

    added some part of code here

--- simpleLine.js
+++ simpleLine.js
@@ -9,3 +9,48 @@ var config = {
         update: update
     }
 }
+
+var follower;
+var path;
+var graphics;
+
+var game = new Phaser.Gamer(config);
+
+function create(){
+    graphics = this.add.graphics();
+
+    followers = { t: 0, vec: new Phaser.Math.Vector2()};
+
+    // The curves do not have to be joined
+    var line1 = new Phaser.Curvers.Line([100, 100, 500, 200])
+    var line2 = new Phaser.Curvers.Line([200, 300, 600, 500])
+
+    path = this.add.path();
+
+
+    path.add(line1);
+    path.add(line2);
+ 
+    this.tween.add({
+        target: followers,
+        t:1,
+        ease: 'Linear',
+        duration: 4000,
+        yoyo: true,
+        repeat: -1
+
+    });
+
+}
+
+
+function update()
+{
+    graphics.clear();
+    graphics.ineStyle(2,0xffffff,1)
+
+    path.draw(graphics)
+    path.getPoint(followers.t, follower.vec)
+
+    
+}
